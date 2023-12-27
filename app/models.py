d0be0a8148e1f60037a2e4898cd455a3254b644a
maquangthien <quangthien1301@gmail.com>
2023-12-27 23:18:30 +0700

    changedata

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from app import db
 from flask_login import UserMixin
@@ -10,7 +10,7 @@ class Role(db.Model):
     role_name = Column(String(50), nullable=False, unique=True)
 
     def __str__(self):
-        return self.name
+        return self.role_name
 
 
 class User(db.Model, UserMixin):
@@ -20,8 +20,9 @@ class User(db.Model, UserMixin):
     email = Column(String(50), nullable=True, unique=True)
     identity = Column(String(20), nullable=True)
     _password = Column(String(100), nullable=False)
-    role_id = Column(Integer, ForeignKey(Role.role_id), nullable=False)
+    role_id = Column(Integer, ForeignKey('role.role_id'), nullable=False)
     role = relationship(Role)
+    tickets = relationship('Ticket', backref='passenger', lazy=True)
 
     def __str__(self):
         return self.name
@@ -38,14 +39,55 @@ class User(db.Model, UserMixin):
         return check_password_hash(self._password, plaintext_password)
 
 
+class Airport(db.Model):
+    airport_code = Column(String(3), primary_key=True)
+    airport_name = Column(String(50), nullable=False, unique=True)
+    airport_location = Column(String(50), nullable=False, unique=True)
+
+    def __str__(self):
+        return self.airport_name
+
+
+class Flight(db.Model):
+    flight_id = Column(Integer, primary_key=True, autoincrement=True)
+    departure_datetime = Column(DateTime, nullable=False)
+    flight_duration = Column(Integer, nullable=False)
+    available_seats_first_class = Column(Integer, nullable=False)
+    available_seats_second_class = Column(Integer, nullable=False)
+    tickets = relationship('Ticket', backref='flight', lazy=True)
+
+
+class IntermediateAirport(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    airport_code = Column(String(3), ForeignKey('airport.airport_code'), nullable=False)
+    flight_id = Column(Integer, ForeignKey('flight.flight_id'), nullable=False)
+    stop_order = Column(Integer, nullable=False)
+    stop_duration = Column(Integer, nullable=False)
+
+
+class Ticket(db.Model):
+    ticket_id = Column(Integer, primary_key=True, autoincrement=True)
+    flight_id = Column(Integer, ForeignKey('flight.flight_id'), nullable=False)
+    passenger_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    ticket_class = Column(String(20), nullable=False)
+    ticket_price = Column(Float, nullable=False)
+    booking_time = Column(DateTime, nullable=False)
+
+
 if __name__ == "__main__":
     from app import app
 
     with app.app_context():
         db.create_all()
+        # Thêm dữ liệu mẫu hoặc các thao tác khác ở đây nếu cần.
+
         # admin_role = Role(role_name='admin')
         # staff_role = Role(role_name='staff')
         # customer_role = Role(role_name='customer')
         #
         # db.session.add_all([admin_role, staff_role, customer_role])
+
+        # f1 = Ariport(airport_code='SGN', airport_name='Sân bay Tân Sân Nhất', airport_location='TP HCM')
+        # f2 = Ariport(airport_code='HAN', airport_name='Sân bay quốc tế Nội Bài', airport_location='Hà Nội')
+        # db.session.add_all([f1, f2])
         # db.session.commit()
